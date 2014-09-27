531897aaa02212c3b5f6d09391d64a0274de6c3e
Mritter26 <Ritter.1442@osu.edu>
2014-09-26 23:57:55 -0400

    Fixed a bug that did not allow jumping. Also, you look down?
    Ya got tits.

--- Source/TutorialFPS/FPSGameMode.cpp
+++ Source/TutorialFPS/FPSGameMode.cpp
@@ -8,7 +8,7 @@ AFPSGameMode::AFPSGameMode(const class FPostConstructInitializeProperties& PCIP)
 	: Super(PCIP)
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FObjectFinder<UBlueprint> PlayerPawnObject(TEXT("Blueprint'/Game/Blueprints/BP_FPSCharacter.BP_FPSCharacter'"));
+	static ConstructorHelpers::FObjectFinder<UBlueprint> PlayerPawnObject(TEXT("Blueprint'/Game/Blueprint/BP_FPSCharacter.BP_FPSCharacter'"));
 	if (PlayerPawnObject.Object != NULL)
 	{
 		DefaultPawnClass = (UClass*)PlayerPawnObject.Object->GeneratedClass;
